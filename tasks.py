611f7e5a6edcfbde91524d9770fcac1eee73c1bb
Hamza Javed <hamzajk202@gmail.com>
2025-09-12 21:04:27 +0500

    assignment2

---
+++ tasks.py
@@ -0,0 +1,27 @@
+print ('Hello, everyone! I am learning Python.' )
+
+Name = 'Hamza Javed'
+Age = 23
+favoriteanimal = 'Lion'
+print (f'''name:{Name} 
+age:{Age} 
+Favorite Animal : {favoriteanimal}''')
+
+print(f''' My name is {Name}, I am {Age} and I love {favoriteanimal}''')
+
+favoritemovie = 'spider man'
+birthyear = 2002
+priceofsnack = 2.5
+print(f'''Favorite Movie: {favoritemovie} 
+Birth Year: {birthyear}
+priceofsnack: {priceofsnack}''')
+
+hobby = 'Reading Books'
+print(f'''My favorite hobby is {hobby} ''')
+
+a =5
+b=8
+c = (a + b)
+print ( c )
+
+
